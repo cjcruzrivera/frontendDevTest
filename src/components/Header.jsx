48b7d4b477ab9d28a9729a65cb40a79c6d469061
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2025-03-26 17:33:08 +0100

    modify header adding breadcrumbs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
   return (
-    <header className="header">
-      <Link to="/">
-        <h1> Mobile Store</h1>
-      </Link>
-      <nav>
-        <p>Carrito: <span id="cart-count">0</span></p>
-      </nav>
+    <header>
+      <div className="header">
+        <Link to="/">
+          <h1>Mobile Store</h1>
+        </Link>
+        <nav>
+          <p>Carrito: <span id="cart-count">0</span></p>
+        </nav>
+      </div>
+      <div className="breadcrumbs">
+        <Link to="/">Inicio</Link>
+        {!isHome && (
+          <>
+            <span className="separator">/</span>
+            <span className="current">Detalles</span>
+          </>
+        )}
+      </div>
     </header>
   );
 }
